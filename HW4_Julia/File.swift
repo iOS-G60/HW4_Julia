d6b07b5324208ca340efb5e8228fe8d270d80034
Шапаренко Юлия <a17@MacBook-Pro-Saparenko.local>
2018-01-31 15:29:08 +0200

    “HW4”

--- HW4_Julia/File.swift
+++ HW4_Julia/File.swift
@@ -7,3 +7,57 @@
 //
 
 import Foundation
+
+class Aircraft {
+
+    let typename: String
+    var weight: Int
+    var passengerCapacity: Int
+    var fuel: Int
+
+    private var preparationsForNextFlight: Bool
+    private var businessClassSeats: Int
+
+
+    init(typename: String,
+         weight: Int,
+         passengerCapacity: Int,
+         fuel: Int,
+         preparationsForNextFlight: Bool = false,
+         businessClassSeats: Int = 30){
+
+        self.typename = typename
+        self.weight = weight
+        self.passengerCapacity = passengerCapacity
+        self.fuel = fuel
+        self.preparationsForNextFlight = preparationsForNextFlight
+        self.businessClassSeats = businessClassSeats
+    }
+
+    func freeSeatsAvailable(_ number: Int)  {
+        passengerCapacity -= number
+    }
+
+    func AtParkingApron() {
+        fuel -= fuel / 1
+        weight -= weight / 2
+        if preparationsForNextFlight {
+            fuel == fuel
+        }
+    }
+
+    func touchDown() {
+        fuel -= fuel / 1
+        weight -= weight / 2
+    }
+
+    private func economyClassSeats() {
+        passengerCapacity -= businessClassSeats
+    }
+
+    func amountOfEconomyClassSeats() {
+        economyClassSeats()
+    }
+
+
+}
